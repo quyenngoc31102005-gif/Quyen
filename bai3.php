506c97a829a151621e20fd114d25cf92d346b840
ngoc quyen <quyenngoc31102005@gmail.com>
2026-05-08 09:14:45 +0700

    BAITAP8.5

--- bai3.php
+++ bai3.php
@@ -1,28 +1,4 @@
-Bài 3. Hiển thị thông tin tour bằng biến PHP
 
-Tạo file: bai2.php
-
-Khai báo các biến:
-
-$tenTour
-$diemDen
-$soNgay
-$soDem
-$giaTour
-Gán giá trị ví dụ:
-
-Tên tour: Tour Đà Lạt 3 ngày 2 đêm
-Điểm đến: Đà Lạt
-Số ngày: 3
-Số đêm: 2
-Giá tour: 3200000
-Trang web phải hiển thị:
-
-Tên tour
-Điểm đến
-Thời lượng
-Giá tour
-Code ví dụ:
 
 <!DOCTYPE html>
 <html lang="vi">
